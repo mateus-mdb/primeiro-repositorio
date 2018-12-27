3c6df17bbd47621dbf92c2810c6fdda5a8cf54b4
Mateus Dias <mdiasbarbosa9@gmail.com>
2018-12-27 10:19:25 -0200

    primeiro commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Portfólio - Mateus Dias Barbosa</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css"/>
+	<script type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="bootstrap.min.js"></script>
+    <script type="text/javascript">
+ (function () {
+ var options = {
+ whatsapp: "+5519996395237", // Número do WhatsApp
+ company_logo_url: "//www.webcreative.com.br/themes/webcreative/images/perfil.jpg", // URL com o logo da empresa
+ greeting_message: "Olá! Gostou do meu trabalho?", // Texto principal
+ call_to_action: "Fale conosco", // Chamada para ação
+ position: "left", // Posição do widget na página 'right' ou 'left'
+ };
+ var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+ s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+ var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+ })();</script>
+</head>
+<!-- INÍCIO DO BODY -->
+<body>
+	
+	<div class="container container-fluid">
+		<div class="background">
+			<img src="images/background.jpg">
+		</div>
+	</div>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap.min.js"></script>
+<script type="text/javascript" src="bootstrap.bundle.js"></script>
+</body>
